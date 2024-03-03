7a394b720b21cefcbff5c0de2c08b4217e1fa927
Andrey Parfenov <asleeppiano@outlook.com>
2024-03-03 23:07:01 +0300

    docs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Мой сайт
+
+in progress
+
+andreyparfenov.com
--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use Mysite\Lib\Translation;
 
 Translation::init();
 
+// TODO: make better router
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if ($uri === '/') {
     HomeController::show();
